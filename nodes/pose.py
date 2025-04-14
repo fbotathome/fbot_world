9fdafbee3585b7a9dd8e352957ada2389ca499fc
vitoranello <vitoranello@gmail.com>
2025-04-14 17:50:54 -0300

    Remove older files from ros1

--- nodes/pose.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import rospy
-from butia_world.plugins import PosePlugin
-
-if __name__ == '__main__':
-  rospy.init_node('pose_plugin')
-  plugin = PosePlugin()
-  plugin.run()
--- scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-from world_plugin import WorldPlugin
-from heartbeat_writer import HeartbeatWriterPlugin
-from heartbeat_reader import HeartbeatReaderPlugin
-from pose import PosePlugin
-from viewer_reader import ViewerReaderPlugin
-from recognition_writer import *
