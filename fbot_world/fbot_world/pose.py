53e8d2985ad58e0b43f0fa6814e8376c995dfeee
vitoranello <vitoranello@gmail.com>
2026-05-08 21:11:14 -0300

    Update a example room yaml; Create a new service msg Create a new function on pose.py to verifiy actual room

--- fbot_world/fbot_world/pose.py
+++ fbot_world/fbot_world/pose.py
@@ -3,12 +3,14 @@
 import rclpy
 import yaml
 import os
+import numpy as np
 from scripts.world_plugin import WorldPlugin
 from fbot_world_msgs.msg import FBOTPoses
-from fbot_world_msgs.srv import GetPose, GetPoseFromSet, GetSets
+from fbot_world_msgs.srv import GetPose, GetPoseFromSet, GetSets, GetRoom
 from geometry_msgs.msg import Pose, Vector3
 from std_msgs.msg  import Empty
 from ament_index_python.packages import get_package_share_directory
+  
 
 def readYamlFile(file_path: str = None):
   """
@@ -46,6 +48,7 @@ class PosePlugin(WorldPlugin):
     self.pose_server = self.create_service(GetPose, '/fbot_world/get_pose', self.getPose)
     self.set_server = self.create_service(GetPoseFromSet, '/fbot_world/get_set', self.getPoseFromSet)
     self.sets_names = self.create_service(GetSets, '/fbot_world/get_groups_names', self.getGroupNames)
+    self.room_server = self.create_service(GetRoom, '/fbot_world/get_room', self.getRoom)
     self.get_logger().info(f"Pose node started!!!")
 
   def readPose(self, group_set: str, key: str):
@@ -80,7 +83,6 @@ class PosePlugin(WorldPlugin):
     """
     self.config_file_name = self.get_parameter('config_file_name').get_parameter_value().string_value
 
-
   def readSize(self, group_set: str, key: str):
     '''
     @brief: Reads size (scale) data for a given key from the Redis database.
@@ -104,12 +106,13 @@ class PosePlugin(WorldPlugin):
     '''
     with self.r.pipeline() as pipe:
       for target in self.targets.keys():
+        if target == 'rooms':
+          break
         for p_id, pose in self.targets[target].items():
           key = str(target)+'/' + p_id + '/' + 'pose'
           pipe.hmset(key, pose)
       pipe.execute()
 
-  
   def setResponseError(self):
     '''
     @brief: A function that set a error pose
@@ -222,6 +225,84 @@ class PosePlugin(WorldPlugin):
     res.response = self.targets.keys()
     return res
   
+  def getRoom(self, req: GetRoom.Request, res: GetRoom.Response):
+    """
+    Executes the state by compare if the point is inside a polygon, and saves the points inside blackboard['inside_polygon'].
+    @return Execution outcome (SUCCEED, ABORT).
+    """
+    for room in self.targets['rooms'].items():
+      self.get_logger().info(f"Room: {room[0]}, Polygon: {room[1]}")
+      self.polygon = np.array(room[1],dtype= np.float32)
+      self.itens_points = req.pose.pose.pose.position
+      if self.is_point_in_area([self.itens_points.x, self.itens_points.y]):
+          res.response = [room[0]]
+          self.get_logger().info(f"Room: {room[0]}")
+          return res
+    res.response = ['None']
+    return res
+      
+  def is_point_in_area(self, point) -> bool:
+        """
+        @brief A function thats verify if a point are inside a area.
+        @param point: Point to verify.
+        @return a bool param, if the intersections are a pair value return true, else, return false.
+        """
+        x, y = point[0], point[1]
+        inside = False
+        
+        n = len(self.polygon)
+        j = n - 1  # Inicia com o último vértice para testar a aresta que fecha o polígono
+        
+        for i in range(n):
+            xi, yi = self.polygon[i][0], self.polygon[i][1]
+            xj, yj = self.polygon[j][0], self.polygon[j][1]
+            
+            # Condição 1: O ponto Y do robô está entre os Ys da parede?
+            # Condição 2: A parede cruza o raio à direita da posição X do robô?
+            intersect = ((yi > y) != (yj > y)) and (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+            
+            if intersect:
+                # Cada vez que cruza uma parede válida, inverte o status (dentro/fora)
+                inside = not inside
+                
+            j = i  # Avança para a próxima aresta
+            
+        return inside
+    
+  # def is_point_in_area(self, point) -> bool:
+  #     """
+  #     @brief A function thats verify if a point are inside a area.
+  #     @param point: Point to verify.
+  #     @return a bool param, if the intersections are a pair value return true, else, return false.
+  #     """
+  #     intersections = 0
+
+  #     for i in range(len(self.polygon)):
+  #         ps1 = self.polygon[i]
+  #         ps2 = None
+  #         if i == len(self.polygon)-1:
+  #             ps2 = self.polygon[0]
+  #         else:
+  #             ps2 = self.polygon[i+1]
+  #         p_end = point.copy()
+  #         p_end[0] = max(self.polygon[:,0])
+  #         self.get_logger().info(f"ps1: {ps1}, ps2: {ps2}, point: {point}, p_end: {p_end}")
+  #         if self.intersect(point,p_end,ps1, ps2):
+  #             intersections+=1
+  #     return intersections % 2 == 1
+      
+  def intersect(self, A,B,C,D):
+      return self.ccw(A,C,D) != self.ccw(B,C,D) and self.ccw(A,B,C) != self.ccw(A,B,D)
+  
+  def ccw(self,A,B,C):
+      return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+
+
+
+
+
+
 
 
 def main(args=None) -> None: 
